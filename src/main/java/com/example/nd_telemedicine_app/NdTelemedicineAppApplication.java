0c88677a2d71345f03cf417a96cab23049a079bf
D7oMxB52 <s3813024@student.rmit.edu.au>
2022-08-14 21:49:51 +1000

    initializing  the repo

---
+++ src/main/java/com/example/nd_telemedicine_app/NdTelemedicineAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.nd_telemedicine_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdTelemedicineAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NdTelemedicineAppApplication.class, args);
+    }
+
+}
