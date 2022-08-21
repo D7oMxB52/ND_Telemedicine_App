134e1ad78320eef3857381a7c33aa6d9fac4ce7d
amyminiter <amyminiter@gmail.com>
2022-08-21 16:24:19 +1000

    Basic set up for API gateway. Commit before attempts to make new Maven microservice module.
    Created RegistrationController.java
    Updated POM dependencies
    Application properties to include routing

---
+++ src/main/java/com/example/nd_telemedicine_app/RegistrationController.java
@@ -0,0 +1,17 @@
+package com.example.nd_telemedicine_app;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping(value="ndTelemedicine") // We're trying to access this endpoint through the API gateway
+public class RegistrationController {
+
+    @RequestMapping(value = "/register/user", method = RequestMethod.POST)
+    //    ndTelemedicine/register is the context path
+    //    ndTelemedicine/register/user is the URL
+    public String registerUser(){
+        return "Registration success";
+    }
+}
