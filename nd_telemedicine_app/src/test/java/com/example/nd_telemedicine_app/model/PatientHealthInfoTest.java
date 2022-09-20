eaef78a424610e33aa3c9465612c164aa5468b13
D7oMxB52 <s3813024@student.rmit.edu.au>
2022-09-20 16:30:12 +1000

    updating the annotations

--- nd_telemedicine_app/src/test/java/com/example/nd_telemedicine_app/model/PatientHealthInfoTest.java
+++ nd_telemedicine_app/src/test/java/com/example/nd_telemedicine_app/model/PatientHealthInfoTest.java
@@ -4,7 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PatientHealthInfoTest.class)
 class PatientHealthInfoTest {
 
     @Test
