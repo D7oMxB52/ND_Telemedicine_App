6e92f2f83328547b4c22248d214f29cd38987445
Amy Miniter <amyminiter@gmail.com>
2022-10-09 09:41:15 +1100

    Created availability table
    Updated schema
    Verified that table correctly created in MySQL workbench
    
    Co-Authored-By: sahsanaly <75010712+sahsanaly@users.noreply.github.com>

--- nd_telemedicine_app/src/main/resources/schema.sql
+++ nd_telemedicine_app/src/main/resources/schema.sql
@@ -37,6 +37,14 @@ CREATE TABLE IF NOT EXISTS booking (
     CONSTRAINT patient_id FOREIGN KEY (patient_id) REFERENCES userServices.users (user_id),
     CONSTRAINT doctor_id FOREIGN KEY (doctor_id) REFERENCES userServices.users (user_id)
 ) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS availability (
+    availability_id INT NOT NULL,
+    doctor_id INT NOT NULL,
+    availability_date DATE NOT NULL,
+    availability_time TIME NOT NULL,
+    PRIMARY KEY (availability_id),
+    FOREIGN KEY (doctor_id) REFERENCES userServices.users (user_id)
+) ENGINE=InnoDB;
 
 
 
