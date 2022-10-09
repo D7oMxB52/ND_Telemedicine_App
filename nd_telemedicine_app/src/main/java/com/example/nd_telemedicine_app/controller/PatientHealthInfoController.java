a1fd8af11e09fd68482ff49a8318b73836e1c173
D7oMxB52 <s3813024@student.rmit.edu.au>
2022-10-10 02:34:32 +1100

    Refactoring the Controller
    Changing the API calls,
    Changing the annotations for APIs
    fixing minor issues with HealthInformationService.java

--- nd_telemedicine_app/src/main/java/com/example/nd_telemedicine_app/controller/PatientHealthInfoController.java
+++ nd_telemedicine_app/src/main/java/com/example/nd_telemedicine_app/controller/PatientHealthInfoController.java
@@ -2,64 +2,45 @@ package com.example.nd_telemedicine_app.controller;
 
 import com.example.nd_telemedicine_app.model.PatientHealthInfo;
 import com.example.nd_telemedicine_app.service.HealthInformationService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/healthinfo")
+@RequestMapping("/ndt/pa-healthinfo")
 public class PatientHealthInfoController {
-
     private HealthInformationService healthInformationService;
-
     public PatientHealthInfoController(HealthInformationService healthInformationService) {
         super();
         this.healthInformationService = healthInformationService;
     }
-
     // build create healthInformation REST API
-    @RequestMapping(value="/save", method = RequestMethod.POST)
-    public ResponseEntity<PatientHealthInfo> saveHealthInfo(@RequestBody PatientHealthInfo patientHealthInfo){
-        return new ResponseEntity<PatientHealthInfo>
-                (healthInformationService.savePatientHealthInfo(patientHealthInfo), HttpStatus.CREATED);
+    @RequestMapping(value="/save", method = RequestMethod.POST, produces="application/json", consumes="application/json")
+    public PatientHealthInfo saveHealthInfo(@RequestBody PatientHealthInfo patientHealthInfo){
+        return healthInformationService.savePatientHealthInfo(patientHealthInfo);
     }
-
     // build get all healthInformation REST API
-    @GetMapping("/all")
+    @RequestMapping(value = "/all", method = RequestMethod.GET,produces="application/json")
     public List<PatientHealthInfo> getAllHealthInfo(){
         return healthInformationService.getAllPatientHealthInfo();
     }
-
-
     // build get healthInformation by id REST API
-    // for example:   http://localhost:8080/api/healthinfo/1
-    @GetMapping("/{id}")
-    public ResponseEntity<PatientHealthInfo> getPatientHealthInfoById(@PathVariable("id") Integer profileId){
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET,produces="application/json")
+    public PatientHealthInfo getPatientHealthInfoById(@PathVariable("id") Integer profileId){
         System.out.print("Hits endpoint");
-        return new ResponseEntity<PatientHealthInfo>
-                (healthInformationService.getPatientHealthInfoById(profileId), HttpStatus.OK);
+        return healthInformationService.getPatientHealthInfoById(profileId);
     }
-
     // build updated health information for existing patients REST API
-    // for example:   http://localhost:8080/api/healthinfo/1
-    @PutMapping("/{id}")
-    public ResponseEntity<PatientHealthInfo> updatePatientHealthInfo(@PathVariable("id") Integer id
-                                                                    ,@RequestBody PatientHealthInfo patientHealthInfo){
-        return new ResponseEntity<PatientHealthInfo>
-                (healthInformationService.updatePatientHealthInfo(patientHealthInfo, id),HttpStatus.OK);
-
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces="application/json", consumes="application/json")
+    public PatientHealthInfo updatePatientHealthInfo(@PathVariable("id") Integer id
+                                       ,@RequestBody PatientHealthInfo patientHealthInfo){
+        System.out.println(patientHealthInfo.getHealthStatus());
+        return healthInformationService.updatePatientHealthInfo(patientHealthInfo, id);
     }
-
     // build delete patient's health information REST API
-    // for example:   http://localhost:8080/api/healthinfo/1
-    @DeleteMapping("{id}")
-    public ResponseEntity<String> deletingPatientHealthInfo(@PathVariable("id") Integer id){
-
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deletingPatientHealthInfo(@PathVariable("id") Integer id){
         //deleting the patient's health information by id
         healthInformationService.deletePatientHealthInfo(id);
-
-        return new ResponseEntity<String>("Patient with ID: "+id+" has been deleted successfully!", HttpStatus.OK);
+        System.out.println("Patient with ID: "+id+" has been deleted successfully!");
     }
 }
