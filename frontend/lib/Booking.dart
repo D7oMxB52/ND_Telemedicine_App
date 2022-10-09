fb593c31c2fc787c15fcb89222f77183f01a3bb3
Luke Mullan s3284243 <s3284243@student.rmit.edu.au>
2022-10-09 12:19:10 +1100

    Booking Calendar sits top centre, displays a unique button for each appointment on the date selected. Shows events in main calendar view

--- frontend/lib/Booking.dart
+++ frontend/lib/Booking.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_widget/flutter_calendar_widget.dart';
 import 'package:intl/intl.dart';
@@ -16,9 +17,9 @@ class Booking extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
+      body: Container(
+        alignment: Alignment.topCenter,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const BookingForm(),
           ],
@@ -38,57 +39,58 @@ class BookingForm extends StatefulWidget {
 }
 
 class BookingFormState extends State<BookingForm> {
+  var _selectedEvents = ['Appointment 1', 'Appointment 2'];
 
   DateTime dateSelected = DateTime.now();
 
+  // Test appointments for calendar
+  EventList<String> events = EventList(
+      events: {
+        DateTime(2022, 10, 9): [
+          'Appointment 1',
+          'Appointment 2',
+        ],
+        DateTime(2022, 10, 19): [
+          'Appointment 1',
+          'Appointment 2',
+        ],
+
+  }
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Container(
+        alignment: Alignment.topLeft,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FlutterCalendar(
                 selectionMode: CalendarSelectionMode.single,
+                events: events,
                 onDayPressed: (DateTime date) {
                   setState(() {
                     dateSelected = date;
                   });
+                  _selectedEvents = events.get(date);
+                  print(_selectedEvents);
                 }),
             Text("Date selected: ${DateFormat('dd MMM, yyyy').format(dateSelected)}"),
-            OutlinedButton(
-              child: Text("Appointment 1"),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return  Booking();
-                }));
-              },
-            ),
-            OutlinedButton(
-              child: Text("Appointment 2"),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return  Booking();
-                }));
-              },
-            ),
-            OutlinedButton(
-              child: Text("Appointment 3"),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return  Booking();
-                }));
-              },
-            ),
-            OutlinedButton(
-              child: Text("Appointment 4"),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return  Booking();
-                }));
-              },
+            Container(
+              child: Column(
+                children: _selectedEvents.map((e) =>
+                    OutlinedButton(
+                      onPressed: () {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        //       return  Booking();
+                        //     }));
+                      },
+                      child: Text(e),
+                    )
+                ).toList(),
+              ),
             ),
           ],
         ),
-      ); // This trailing comma makes auto-formatting nicer for build methods.
+    );    // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
