5bb794285b7a74690c5863134aa835849de67644
sahsanaly <s3736294@student.rmit.edu.au>
2022-10-10 11:58:53 +1100

    Created Prescription Microservice
    Created prescription micro service for users
    Created Controller, Model, Service and Repository
    Created the table for prescription in the database

--- nd_telemedicine_app/src/main/resources/schema.sql
+++ nd_telemedicine_app/src/main/resources/schema.sql
@@ -38,15 +38,17 @@ CREATE TABLE IF NOT EXISTS booking (
     CONSTRAINT patient_id FOREIGN KEY (patient_id) REFERENCES userServices.users (user_id),
     CONSTRAINT doctor_id FOREIGN KEY (doctor_id) REFERENCES userServices.users (user_id)
 ) ENGINE=InnoDB;
--- CREATE TABLE IF NOT EXISTS availability (
---     availability_id INT NOT NULL,
---     doctor_id INT NOT NULL,
---     availability_date DATE NOT NULL,
---     availability_time TIME NOT NULL,
---     availability_end_time TIME NOT NULL,
---     PRIMARY KEY (availability_id),
---     FOREIGN KEY (doctor_id) REFERENCES userServices.users (user_id)
--- ) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS prescription (
+    prescription_id INT NOT NULL AUTO_INCREMENT,
+    patient_id INT NOT NULL,
+    doctor_id INT NOT NULL,
+    medicine_name varchar(255) DEFAULT NULL,
+    dose varchar(255) DEFAULT NULL,
+    instructions varchar(255) DEFAULT NULL,
+    PRIMARY KEY (prescription_id),
+    FOREIGN KEY (patient_id) REFERENCES userServices.users (user_id),
+    FOREIGN KEY (doctor_id) REFERENCES userServices.users (user_id)
+) ENGINE=InnoDB;
 
 
 
